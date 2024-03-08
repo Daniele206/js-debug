f71ca2a2002ef50bd5790782bd7fd8f7c3302a85
Daniele206 <danieledeieso2004@gmail.com>
2024-03-08 16:09:06 +0100

    [add] bundle 1 es4

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -39,7 +39,6 @@ function loopToFive() {
     for (let i = 0; i <= 5; i++) {
         console.log(i);
         number = i;
-        console.log(number);
     };
     return number
 };
@@ -47,19 +46,23 @@ function loopToFive() {
 console.log(loopToFive());
 // 1 - Stampa in console i numeri da 0 a 5.
 // 2 - C'é un errore di sintassi all'interno delle parentesi tonde del for, i vari elementi vanno separati da ; non da , e per fare un loop fino a 5 deve esserci <= non <.
-// 3 - C'é un erroe di logica, la funzione viene creata ma mai utilizzata, e in piú manca una variabile che ritorni alla funzione.
+// 3 - C'é un erroe di logica, la funzione viene creata ma mai utilizzata, e in piú manca una variabile che ritorni che ci cacci il dato dal for e un return per far uscire il data dalla funzione.
 
 
 
-// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
-//     }
-// }
-// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
+        }
+    }
+    return evenNumbers;
+}
+console.log(displayEvenNumbers()); // dovrebbe restituire [2,4,6,8]\
+
+// 1 - Prende un array di numeri e ne restituisce solo quelli pari.
+// 2 - Ci sono errori di sintassi, il controllo pari o dispari si fa con (num % 2 == 0 oppure num % 2 === 0) non va bene il singolo uguale, non vanno messi ; dopo le tonde di un if, e dopo l'ultimo elemento nelle tonde di un for, nelle tonde del for il controllo va fatto su (i < numbers.lenght e non i < numbers.lenght - 1).
+// 3 - Non ce un consol log che ci permetta di vere cosa fa la funzione, il return deve stare nella funzione ma fuori dal for, manca [i] dopo numbers nell'if, al posto di (i) nel push va messo (numbers[i]).
