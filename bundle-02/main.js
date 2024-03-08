a64f2480fcffbe7813633dbf5ba52ec426413bc0
Daniele206 <danieledeieso2004@gmail.com>
2024-03-08 16:34:18 +0100

    [add] bundle2 es5

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -77,59 +77,32 @@ checkAccess();
 // 3 - Non ci sono errori di logica.
 
 
-// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
-// function checkAccessImproved() {
-//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
-//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-//     let grantAccess = 'false';
+    let grantAccess = false;
 
-//     for (let i = 0; i < addresses.length; i++) {
-//         const email = addresses[i];
+    for (let i = 0; i < addresses.length; i++) {
+        const email = addresses[i];
 
-//         if (userEmail.length > 5) {
+        if (userEmail.length > 5) {
+            if (email === userEmail) {
+                grantAccess = true;
+            }
+        }
 
-//             if (email === userEmail) {
-//                 grantAccess = 'true';
+        if(grantAccess){
+            console.log('Accesso consentito!');
+        } else {
+            console.log('Accesso negato!');
+        }
+    }
+};
+checkAccessImproved();
 
-//             }
-
-//         }
-
-//         if (grantAccess) {
-//             console.log('Accesso consentito!');
-//         } else {
-//             console.log('Accesso negato!');
-//         }
-//     }
-//     checkAccessImproved();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 1 - Controlla se la mail inserita àe nella lista delle mail che hanno l'accesso, é invia un messaggio alla console.
+// 2 - Maca la parentesi graffa alla fine della funzione.
+// 3 - grantAccess va dichiarato con true o false senza virgolette se vogliamo mantenere un if di quel tipo.
